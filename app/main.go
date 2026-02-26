489808f4c1271f6a0cb61789747b503a21e69876
rafidoth <rafiulhasan803@gmail.com>
2026-02-27 02:30:43 +0600

    implement echo

--- app/main.go
+++ app/main.go
@@ -11,6 +11,10 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
+func extractArguments(wholeCommand string) []string {
+	return strings.Split(wholeCommand, " ")
+}
+
 func main() {
 	for {
 		fmt.Print("$ ")
@@ -20,11 +24,20 @@ func main() {
 			log.Fatal(err)
 		}
 
-		command = strings.TrimSpace(command)
-		if command == "exit" {
+		if len(command) == 0 {
 			break
 		}
 
-		fmt.Printf("%v: command not found\n", command[:len(command)])
+		command = strings.TrimSpace(command)
+		commandWithExtractedArgs := extractArguments(command)
+
+		if command == "exit" {
+			break
+		} else if commandWithExtractedArgs[0] == "echo" {
+			fmt.Println(strings.Join(commandWithExtractedArgs[1:], " "))
+		} else {
+			fmt.Printf("%v: command not found\n", command)
+		}
+
 	}
 }
