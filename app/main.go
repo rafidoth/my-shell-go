1b529abbfcf19a698867dacfd462653433bc62f3
rafidoth <rafiulhasan803@gmail.com>
2026-02-27 02:02:56 +0600

    implement a repl

--- app/main.go
+++ app/main.go
@@ -12,12 +12,14 @@ var _ = fmt.Print
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	fmt.Print("$ ")
-	reader := bufio.NewReader(os.Stdin)
-	command, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
+		fmt.Print("$ ")
+		reader := bufio.NewReader(os.Stdin)
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	fmt.Printf("%v: command not found", command[:len(command)-1])
+		fmt.Printf("%v: command not found\n", command[:len(command)-1])
+	}
 }
