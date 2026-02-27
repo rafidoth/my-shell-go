f007dacab018d8cb0d18180ae87d0a782cfef2c3
rafidoth <rafiulhasan803@gmail.com>
2026-02-27 19:20:58 +0600

    redirect stdout

--- app/utils.go
+++ app/utils.go
@@ -4,18 +4,16 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"slices"
 	"strings"
 	"unicode"
 )
 
 func extractArguments(wholeCommand string) []string {
-	// s := singleQuoteHandler(wholeCommand)
-	// fmt.Println(s)
-	// d := doubleQuoteHandler(wholeCommand)
-	// fmt.Println(d)
 	splitted := parseCommand(wholeCommand)
 	return splitted
 }
+
 func parseCommand(input string) []string {
 	var parsed []string
 	var builder strings.Builder
@@ -64,94 +62,27 @@ func parseCommand(input string) []string {
 	return parsed
 }
 
-func commandParser(quoteHandled string) []string {
-	// fmt.Println(singleQuoteHandledCmd)
-	var parsed []string
-
-	var builder strings.Builder
-	insideQuotes := false
-	doubleQuote := false
-	singleQuote := false
-	for _, r := range quoteHandled {
-		switch {
-		case r == '"' && !singleQuote:
-			insideQuotes = !insideQuotes
-			doubleQuote = !doubleQuote
-		case r == '\'' && !doubleQuote:
-			insideQuotes = !insideQuotes
-			singleQuote = !singleQuote
-		case unicode.IsSpace(r) && !insideQuotes:
-			parsed = append(parsed, builder.String())
-			builder.Reset()
-		default:
-			builder.WriteRune(r)
-		}
-	}
-	if builder.Len() > 0 {
-		parsed = append(parsed, builder.String())
-	}
-	return parsed
-}
-
-func doubleQuoteHandler(wholeCommand string) string {
-	var builder strings.Builder
-	singleQuote := false
-	doubleQuote := false
-	lastRuneWasSpace := false
-	escaped := false
-
-	input := strings.TrimSpace(wholeCommand)
-	for _, r := range input {
-		if escaped {
-			builder.WriteRune(r)
-			escaped = false
-			lastRuneWasSpace = false
-			continue
-		}
-
-		switch {
-		case r == '\\' && !singleQuote:
-			escaped = true
-
-		case r == '"' && !singleQuote:
-			doubleQuote = !doubleQuote
-			builder.WriteRune(r)
-			lastRuneWasSpace = false
-
-		case r == '\'' && !doubleQuote:
-			singleQuote = !singleQuote
-			builder.WriteRune(r)
-			lastRuneWasSpace = false
-
-		case unicode.IsSpace(r):
-			if singleQuote || doubleQuote {
-				builder.WriteRune(r)
-			} else {
-				if lastRuneWasSpace {
-					continue
-				} else {
-					builder.WriteRune(r)
-					lastRuneWasSpace = true
-				}
-			}
-
-		default:
-			builder.WriteRune(r)
-			lastRuneWasSpace = false
-		}
-	}
-
-	if escaped {
-		builder.WriteRune('\\')
-	}
-
-	return builder.String()
-}
-
 func execute(primary string, args []string) {
+	gT := slices.Contains(args, ">")
+	gT1 := slices.Contains(args, "1>")
+	if gT || gT1 {
+		var filenameIndex int
+		if gT {
+			filenameIndex = slices.Index(args, ">") + 1
+		} else if gT1 {
+			filenameIndex = slices.Index(args, "1>") + 1
+		}
+		if filenameIndex < len(args) {
+			file, err := os.OpenFile(args[filenameIndex], os.O_RDONLY|os.O_CREATE, 0644)
+			if err != nil {
+			}
+			file.Close()
+		}
+	}
+
 	cmd := exec.Command(primary, args...)
 	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	// cmd.Stderr = os.Stderr
 
 	err := cmd.Run()
 	if err != nil {
