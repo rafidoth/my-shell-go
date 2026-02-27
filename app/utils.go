7d81bb32343e49e00cd63933ec3033cbec6866db
rafidoth <rafiulhasan803@gmail.com>
2026-02-27 17:44:06 +0600

    double quote 1

--- app/utils.go
+++ app/utils.go
@@ -9,20 +9,30 @@ import (
 )
 
 func extractArguments(wholeCommand string) []string {
-	splitted := commandParser(singleQuoteHandler(wholeCommand))
+	// s := singleQuoteHandler(wholeCommand)
+	// fmt.Println(s)
+	d := doubleQuoteHandler(wholeCommand)
+	fmt.Println(d)
+	splitted := commandParser(d)
 	return splitted
 }
 
-func commandParser(singleQuoteHandledCmd string) []string {
+func commandParser(quoteHandled string) []string {
 	// fmt.Println(singleQuoteHandledCmd)
 	var parsed []string
 
 	var builder strings.Builder
 	insideQuotes := false
-	for _, r := range singleQuoteHandledCmd {
+	doubleQuote := false
+	singleQuote := false
+	for _, r := range quoteHandled {
 		switch {
-		case r == '\'':
+		case r == '"' && !singleQuote:
 			insideQuotes = !insideQuotes
+			doubleQuote = !doubleQuote
+		case r == '\'' && !doubleQuote:
+			insideQuotes = !insideQuotes
+			singleQuote = !singleQuote
 		case unicode.IsSpace(r) && !insideQuotes:
 			parsed = append(parsed, builder.String())
 			builder.Reset()
@@ -36,6 +46,35 @@ func commandParser(singleQuoteHandledCmd string) []string {
 	return parsed
 }
 
+func doubleQuoteHandler(wholeCommand string) string {
+	var builder strings.Builder
+	insideQuotes := false
+	lastRuneWasSpace := false
+	input := strings.TrimSpace(wholeCommand)
+	for _, r := range input {
+		switch {
+		case r == '"':
+			insideQuotes = !insideQuotes
+			builder.WriteRune(r)
+			lastRuneWasSpace = false
+		case unicode.IsSpace(r):
+			if insideQuotes {
+				builder.WriteRune(r)
+			} else {
+				if lastRuneWasSpace {
+					continue
+				} else {
+					builder.WriteRune(r)
+					lastRuneWasSpace = true
+				}
+			}
+		default:
+			builder.WriteRune(r)
+			lastRuneWasSpace = false
+		}
+	}
+	return builder.String()
+}
 func singleQuoteHandler(wholeCommand string) string {
 	var builder strings.Builder
 	insideQuotes := false
