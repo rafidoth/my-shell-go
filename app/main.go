c1a4cb5c4dc61576c96fbf26ddf3db46303a957a
rafidoth <rafiulhasan803@gmail.com>
2026-03-04 07:28:36 +0600

    autocomplete try 1

--- app/main.go
+++ app/main.go
@@ -1,20 +1,89 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strings"
+
+	"golang.org/x/term"
 )
 
+func strProcessor(line []byte) string {
+	return strings.TrimSpace(string(line))
+}
+
+func readRawInput(fd int) string {
+	oldState, err := term.MakeRaw(fd)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error :  setting terminal : %v \n", err)
+	}
+	defer term.Restore(fd, oldState)
+
+	var line []byte
+	buf := make([]byte, 1)
+
+	for {
+		n, err := os.Stdin.Read(buf)
+		if err != nil || n == 0 {
+			break
+		}
+		char := buf[0]
+
+		if char == '\n' || char == '\r' {
+			fmt.Print("\r\n")
+			return strProcessor(line)
+		}
+
+		if char == '\t' {
+			partial := strProcessor(line)
+			completed := autocomplete(partial)
+			if len(partial) < len(completed) {
+				fmt.Print(completed[len(partial):])
+				line = []byte(completed)
+			}
+			continue
+		}
+
+		if char == 127 || char == 8 {
+			if len(line) > 0 {
+				line = line[:len(line)-1]
+				fmt.Print("\b \b")
+			}
+			continue
+		}
+
+		if char == 3 {
+			line = nil
+			fmt.Print("\r\n^C")
+			break
+		}
+
+		line = append(line, char)
+		fmt.Print(string(char))
+	}
+
+	cmdString := string(line)
+	command := strings.TrimSpace(cmdString)
+	return command
+}
+
+func autocomplete(partial string) string {
+	builtins := []string{"echo", "exit"}
+	for _, str := range builtins {
+		if strings.HasPrefix(str, partial) {
+			return str
+		}
+	}
+	return ""
+}
+
 func main() {
+	fd := int(os.Stdin.Fd())
 	for {
 		fmt.Print("$ ")
-		reader := bufio.NewReader(os.Stdin)
-		command, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
+		command := readRawInput(fd)
+		if command == "exit" {
+			break
 		}
 
 		if len(command) == 0 {
@@ -23,8 +92,6 @@ func main() {
 
 		command = strings.TrimSpace(command)
 		commandWithExtractedArgs := extractArguments(command)
-		// fmt.Println(commandWithExtractedArgs)
-		// fmt.Println(len(commandWithExtractedArgs))
 		primary := commandWithExtractedArgs[0]
 		args := commandWithExtractedArgs[1:]
 
@@ -45,5 +112,6 @@ func main() {
 		} else {
 			execute(primary, args)
 		}
+		// fmt.Println(command)
 	}
 }
